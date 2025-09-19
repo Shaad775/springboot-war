e53fb1b2bcebe460ef57668555fee3e4acbc37f8
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-09-19 13:12:27 +0530

    Remaining things I am pushing

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.DataClass;
@@ -9,6 +10,7 @@ import com.example.demo.model.DataClass;
 public class MainController {
 
 
+    @GetMapping
     public ResponseEntity<DataClass> greet()
     {
         DataClass dataClass = new DataClass();
